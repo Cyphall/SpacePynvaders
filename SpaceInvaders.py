dad107c9efa3db1abd29b19f025d3650d449ba44
Cyphall <cyphallbraus@gmail.com>
2018-07-18 16:06:53 +0200

    Restructuration des classes et de la main loop

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -4,99 +4,34 @@ import time
 import os
 
 #=== <Classes> ===#
-class display():
+class Display():
 	def __init__(self, size=(640, 480)):
 		self.display = pygame.display.set_mode(size)
 		pygame.display.set_caption("Space Pynvaders")
-		self.aliens = []
-		self.shot = None
-		self.player = None
-		self.sounds = soundInit()
-	
-	
-	def initAliens(self):
-		for y in range(5):
-			for x in range(11):
-				if (y == 0 or y == 1):
-					temp = alien("sprites/10")
-					temp.setPos(x=x*60, y=250-y*60)
-					self.aliens.append(temp)
-				elif (y == 2 or y == 3):
-					temp = alien("sprites/20")
-					temp.setPos(x=x*60, y=250-y*60)
-					self.aliens.append(temp)
-				elif (y == 4):
-					temp = alien("sprites/30")
-					temp.setPos(x=x*60, y=250-y*60)
-					self.aliens.append(temp)
-	
-	
-	def playMusic(self, music):
-		self.sounds[music].play()
-	
-	
-	def hit(self, alien):
-		alien.actual = 2
-		self.sounds["boom"].play()
-		self.shot = None
-	
-	
-	def delAlien(self, alien):
-		self.aliens.remove(alien)
-	
-	def initPlayer(self, path):
-		self.player = player(path)
-	
-	
-	def miss(self):
-		self.shot = None
-	
-	
-	def fire(self):
-		if (self.shot is None):
-			self.sounds["shoot"].play()
-			self.shot = shot("sprites/shot.png", (self.player.getPos()["x"]+24, self.player.getPos()["y"]-12))
 	
 	
 	def render(self):
 		self.display.fill((pygame.Color("black")))
-		for s in self.aliens:
+		global aliens
+		global player
+		global shot
+		for s in aliens:
 			s.render(self.display)
-		if (self.player is not None):
-			self.player.render(self.display)
-		if (self.shot is not None):
-			self.shot.render(self.display)
+		if (player is not None):
+			player.render(self.display)
+		if (shot is not None):
+			shot.render(self.display)
 		pygame.display.flip()
-	
-	
-	def getAliens(self, copy=False):
-		if (copy):
-			return self.aliens[:]
-		return self.aliens
-	
-	
-	def getPlayer(self):
-		return self.player
-	
-	
-	def getShot(self):
-		return self.shot
-	
-	
-	def shotExists(self):
-		if (self.shot is not None):
-			return True
-		return False
 	
 	
 	def getDisplaySize(self):
 		return {"x":pygame.display.Info().current_w, "y":pygame.display.Info().current_h} 
 
 
-class sprite(object):
+class Sprite():
 	def __init__(self, path, pos=(0, 0)):
 		try:
-			self.sprite = pygame.image.load(path).convert_alpha()
+			self.sprite = pygame.image.load("sprites/"+str(path)).convert_alpha()
 			self.pos = {"x":pos[0], "y":pos[1]}
 			self.size = {"x":self.sprite.get_size()[0], "y":self.sprite.get_size()[1]}
 			logging.info("Sprite successfully loaded from "+path)
@@ -111,7 +46,6 @@ class sprite(object):
 			self.pos["y"] = y
 		
 	
-	
 	def getPos(self):
 		return self.pos
 	
@@ -124,7 +58,7 @@ class sprite(object):
 		display.blit(self.sprite, (self.pos["x"], self.pos["y"]))
 	
 
-class alien(sprite):
+class Alien(Sprite):
 	def __init__(self, path, pos=(0, 0)):
 		try:
 			self.sprite = [pygame.image.load(path+"_0.png").convert_alpha(), pygame.image.load(path+"_1.png").convert_alpha(), pygame.image.load("sprites/boom.png").convert_alpha()]
@@ -136,20 +70,20 @@ class alien(sprite):
 			logging.error(e)
 	
 	
-	def move(self, dir, display):
+	def move(self, direction, display):
 		if (self.actual == 1):
 			self.actual = 0
 		elif (self.actual == 0):
 			self.actual = 1
-		if (dir == "l"):
+		if (direction == "l"):
 			self.pos["x"] -= 8
 			if (self.pos["x"] <= 0):
 				return True
-		elif (dir == "r"):
+		elif (direction == "r"):
 			self.pos["x"] += 8
 			if (self.pos["x"]+self.size["x"] >= display.getDisplaySize()["x"]):
 				return True
-		elif (dir == "d"):
+		elif (direction == "d"):
 			self.pos["y"] += 40
 		return False
 	
@@ -158,21 +92,31 @@ class alien(sprite):
 		display.blit(self.sprite[self.actual], (self.pos["x"], self.pos["y"]))
 
 
-class player(sprite):
-	def move(self, dir, display):
-		if (dir == "l"):
+class Player(Sprite):
+	def move(self, direction, display):
+		if (direction == "l"):
 			self.pos["x"] -= 4
 			if (self.pos["x"] < 0):
 				self.pos["x"] = 0
-		elif (dir == "r"):
+		elif (direction == "r"):
 			self.pos["x"] += 4
 			if (self.pos["x"]+self.size["x"] > display.getDisplaySize()["x"]):
 				self.pos["x"] = display.getDisplaySize()["x"]-self.size["x"]
+	def fire(self):
+		global shot
+		if (shot is None):
+			playSound("shoot")
+			shot = Shot("shot.png", (self.pos["x"]+24, self.pos["y"]-12))
 
 
-class shot(sprite):
+class Shot(Sprite):
 	def move(self):
 		self.pos["y"] -= 16
+		if (self.pos["y"] < 0):
+			global shot
+			shot = None
+			return True
+		return False
 #=== </Classes> ===#
 
 
@@ -194,118 +138,144 @@ def soundInit():
 		sounds[os.path.splitext(file)[0]] = pygame.mixer.Sound("sounds/"+file)
 		sounds[os.path.splitext(file)[0]].set_volume(0.05)
 	return sounds
+
+def playSound(sound):
+	global sounds
+	sounds[sound].play()
+
+def hit(alien):
+	alien.actual = 2
+	playSound("boom")
+	global shot
+	shot = None
 #=== </Fonctions> ===#
 
 
 #=== <Main> ===#
-def main():
-	logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-	pygame.mixer.pre_init(44100, -16, 1, 512)
-	pygame.init()
-	size = (712, 740)
-	d = display(size=size)
-	d.initAliens()
-	d.initPlayer("sprites/player.png")
-	d.getPlayer().setPos(y=size[1]-(d.getPlayer().getSize()["y"]))
-	d.render()
-	fps = 60
-	
-	running = True
-	
-	dir = "r"
-	nextDir = None
-	turning = False
-	
-	toDelete = None
-	explosion = False
-	
-	music = 1
-	timing = 0
-	
-	while (running):
-		for alien in d.getAliens(copy=True):
-			for event in pygame.event.get():
-				if (event.type == pygame.QUIT):
-					running = False
-			if (pygame.key.get_pressed()[97] == True or pygame.key.get_pressed()[276] == True):
-				d.getPlayer().move("l", d)
-			if (pygame.key.get_pressed()[100] == True or pygame.key.get_pressed()[275] == True):
-				d.getPlayer().move("r", d)
-			
-			
-			if (alien.move(dir, d) and turning == False):
-				turning = True
-			
-			if (explosion):
-				for i in range(20):
-					timing += 1
-					if (timing >= round(len(d.getAliens())/1.5)+5):
-						timing = 0
-						d.playMusic("music"+str(music))
-						music += 1
-						if (music == 5):
-							music = 1
-					for event in pygame.event.get():
-						if (event.type == pygame.QUIT):
-							running = False
-					if (pygame.key.get_pressed()[97] == True or pygame.key.get_pressed()[276] == True):
-						d.getPlayer().move("l", d)
-					if (pygame.key.get_pressed()[100] == True or pygame.key.get_pressed()[275] == True):
-						d.getPlayer().move("r", d)
-					d.render()
-					time.sleep(1/fps)
+# Initialisation du logger #
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+# Initialisation de pygame #
+pygame.mixer.pre_init(44100, -16, 1, 512)
+pygame.init()
+
+# Initialisation des constantes #
+FPS = 60
+
+# Initialisation des variables #
+direction = "r"
+turning = False
+turningOnNextLoop = False
+
+toDelete = None
+explosion = False
+
+music = 1
+timing = 0
+
+running = True
+
+shot = None
+
+i = 0 # index du prochain alien à bouger
+
+explosionFrame = 0
+
+# initialisation des sons #
+sounds = soundInit()
+
+# Initialisation de la fenêtre #
+disp = Display(size=(712, 740))
+
+# Initialisation des aliens #
+aliens = []
+for y in range(5):
+	for x in range(11):
+		if (y == 0 or y == 1):
+			temp = Alien("sprites/10")
+			temp.setPos(x=x*60, y=250-y*60)
+			aliens.append(temp)
+		elif (y == 2 or y == 3):
+			temp = Alien("sprites/20")
+			temp.setPos(x=x*60, y=250-y*60)
+			aliens.append(temp)
+		elif (y == 4):
+			temp = Alien("sprites/30")
+			temp.setPos(x=x*60, y=250-y*60)
+			aliens.append(temp)
+
+# Initialisation du joueur #
+player = Player("player.png")
+player.setPos(y=disp.getDisplaySize()["y"]-(player.getSize()["y"]))
+
+# Premier rendu #
+disp.render()
+
+# Boucle principale #
+while (running):
+	while (i < len(aliens)):
+		# Évènements #
+		for event in pygame.event.get():
+			if (event.type == pygame.QUIT):
+				running = False
+		if (pygame.key.get_pressed()[97] == True or pygame.key.get_pressed()[276] == True):
+			player.move("l", disp)
+		if (pygame.key.get_pressed()[100] == True or pygame.key.get_pressed()[275] == True):
+			player.move("r", disp)
+		if (pygame.key.get_pressed()[32] == True and not explosion):
+			player.fire()
+		
+		# Gestion du déplacement des aliens et des explosions #
+		if (not explosion):
+			if (turning):
+				aliens[i].move("d", disp)
+			else:
+				if (aliens[i].move(direction, disp)):
+					turningOnNextLoop = True
+		else:
+			explosionFrame += 1
+			i -= 1
+			if (explosionFrame > 20):
 				explosion = False
-				d.delAlien(toDelete)
+				aliens.remove(toDelete)
 				toDelete = None
-			
-			timing += 1
-			if (timing >= round(len(d.getAliens())/1.5)+5):
-				timing = 0
-				d.playMusic("music"+str(music))
-				music += 1
-				if (music == 5):
-					music = 1
-			
-			if (pygame.key.get_pressed()[32] == True):
-				d.fire()
-			
-			if (d.shotExists()):
-				d.getShot().move()
-				if (d.getShot().getPos()["y"] < 0):
-					d.miss()
-				else:
-					for al in d.getAliens():
-						if (collision(d.getShot(), al)):
-							d.hit(al)
-							toDelete = al
-							explosion = True
-							break
-			
-			
-			
-			
-			d.render()
-			time.sleep(1/fps)
+				explosionFrame = 0
+				i -= 1
+		
+		# Gestion de la musique #
+		timing += 1
+		if (timing >= round(len(aliens)/1.5)+5):
+			timing = 0
+			playSound("music"+str(music))
+			music += 1
+			if (music > 4):
+				music = 1
+		
+		# Gestion des tirs #
+		if (shot is not None):
+			if(not shot.move()):
+				for al in aliens:
+					if (collision(shot, al)):
+						hit(al)
+						toDelete = al
+						explosion = True
+						break
 		
 		
-		if (len(d.getAliens()) == 0):
-			running = False
-		
-		if (turning and nextDir is None):
-			if (dir == "r"):
-				nextDir = "l"
-			elif (dir == "l"):
-				nextDir = "r"
-			dir = "d"
-		elif (turning and nextDir is not None):
-			dir = nextDir
-			nextDir = None
-			turning = False
-		
+		disp.render()
+		time.sleep(1/FPS)
+		i += 1
+	
+	if (turning):
+		if (direction == "l"):
+			direction = "r"
+		else:
+			direction = "l"
+		turning = False
+	
+	if (turningOnNextLoop):
+		turning = True
+		turningOnNextLoop = False
+	i = 0
+	
 #=== </Main> ===#
-
-
-#=== <Init> ===#
-if (__name__ == "__main__"):
-	main()
-#=== </Init> ===#
